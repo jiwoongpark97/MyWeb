0480c16a789eddd645c03a8c514dd2a1adc83947
JP <qkrwldnd97@gmail.com>
2024-05-06 22:37:33 -0700

    Initial setup

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import { Container, Navbar as NavbarBs } from 'react-bootstrap'
+
+export function Navbar() {
+    return (
+        <NavbarBs className='bg-white'>
+            <Container>Nav</Container>
+        </NavbarBs>
+    )
+}
