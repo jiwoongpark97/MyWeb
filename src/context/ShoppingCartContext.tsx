4e02c920c80a1411fc627b071b8520093c3bc259
JP <qkrwldnd97@gmail.com>
2024-05-08 00:24:04 -0700

    implemented localstorage to store the data
    It keeps data from refreshing the page.

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, createContext, useContext, useState } from "react"
 import { ShoppingCart } from "../components/ShoppingCart"
+import { useLocalStorage } from "../hooks/useLocalStorage"
 
 type ShoppingCartProviderProps = {
     children: ReactNode
@@ -30,7 +31,7 @@ export function useShoppingCart() {
 
 export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
     const [isOpen, setIsOpen] = useState(false);
-    const [cartItems, setCartItems] = useState<CartItem[]>([])
+    const [cartItems, setCartItems] = useLocalStorage<CartItem[]>("shopping-cart", [])
 
     const cartQuantity = cartItems.reduce((quantity, item) => item.quantity + quantity, 0)
     const openCart = () => setIsOpen(true)
@@ -81,6 +82,6 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
 
     return <ShoppingCartContext.Provider value={{ getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart, cartItems, cartQuantity, openCart, closeCart }}>
         {children}
-        <ShoppingCart isOpen={isOpen}/>
+        <ShoppingCart isOpen={isOpen} />
     </ShoppingCartContext.Provider>
 }
