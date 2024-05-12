35e53069afb03ff21a2989c7dc8ac3b04eb1d024
JP <jiwoongpark97@gmail.com>
2024-05-12 00:24:54 -0700

    Added the first default slide on home screen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,23 @@
 import { Carousel } from 'react-bootstrap';
 import projectList from '../data/projects.json'
+import firstSlide from '../../public/imgs/default.jpg'
 
 
 export function Home() {
   return (
     <div className="m-auto d-flex justify-content-center align-items-center">
-      <Carousel style={{ maxWidth: '40%', minWidth: '500px' }}>
+      <Carousel interval={5000} style={{ maxWidth: '40%', minWidth: '500px' }}>
+        <Carousel.Item>
+          <img
+            className="w-100"
+            src={firstSlide}
+            alt="Image One"
+          />
+          <Carousel.Caption className='text-white'>
+            <h1>Projects</h1>
+            <h2>My side projects</h2>
+          </Carousel.Caption>
+        </Carousel.Item>
         {projectList.map(item => (
           <Carousel.Item>
             <img
