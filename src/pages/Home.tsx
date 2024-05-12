f9fddbf044728f072bbb2934c81352e36b0954f1
JP <jiwoongpark97@gmail.com>
2024-05-11 23:41:43 -0700

    Slideshow implemented in the home page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,44 +1,25 @@
 import { Carousel } from 'react-bootstrap';
-import Argus from '../../public/imgs/Argus.png'
-import Campus_Buddy from '../../public/imgs/Campus_Buddy.png'
-import dictionaryApp from '../../public/imgs/dictionaryApp.jpg'
+import projectList from '../data/projects.json'
+
 
 export function Home() {
   return (
-    <Carousel style={{ width: '400px', height: '600px' }}>
-      <Carousel.Item style={{ height: '600px' }}>
-        <img
-          className="w-100"
-          src={Argus}
-          alt="Image One"
-        />
-        <Carousel.Caption className='text-dark' style={{ position: 'relative' }}>
-          <h3>Argus</h3>
-          <p>Few Projects I have done.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item style={{ height: '400px' }}>
-        <img
-          className="w-100"
-          src={Campus_Buddy}
-          alt="Image One"
-        />
-        <Carousel.Caption className='text-dark' style={{ position: 'relative' }}>
-          <h3>Campus Buddy</h3>
-          <p>Few Projects I have done.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item style={{ height: '400px' }}>
-        <img
-          className="w-100"
-          src={dictionaryApp}
-          alt="Image One"
-        />
-        <Carousel.Caption className='text-dark' style={{ position: 'relative' }}>
-          <h3>Dictionary</h3>
-          <p>Few Projects I have done.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+    <div className="m-auto d-flex justify-content-center align-items-center">
+      <Carousel style={{ maxWidth: '40%', minWidth: '500px' }}>
+        {projectList.map(item => (
+          <Carousel.Item>
+            <img
+              className="w-100"
+              src={item.src}
+              alt="Image One"
+            />
+            <Carousel.Caption className='text-dark' style={{ position: 'relative', right: 0, left: 0 }}>
+              <h3>{item.title}</h3>
+              <p>{item.description}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </div>
   );
 }
