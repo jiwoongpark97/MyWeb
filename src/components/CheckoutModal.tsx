c7f5f2e91cb2a0b3de81ff0cd856cb8b3d825044
JP <jiwoongpark97@gmail.com>
2024-05-12 19:08:43 -0700

    added the items on the checkout modal

--- src/components/CheckoutModal.tsx
+++ src/components/CheckoutModal.tsx
@@ -1,5 +1,9 @@
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import { useShoppingCart } from '../context/ShoppingCartContext';
+import { CartItem } from './CartItem';
+import { formatCurrency } from '../utilities/formatCurrency';
+import storeItems from '../../src/data/items.json'
 
 type CheckoutModalProps = {
     show: boolean
@@ -8,12 +12,15 @@ type CheckoutModalProps = {
 
 
 export function CheckoutModal({ show, onHide }: CheckoutModalProps) {
+    const { cartItems } = useShoppingCart();
+
     return (
         <Modal
             show={show}
             onHide={onHide}
             size="lg"
             aria-labelledby="contained-modal-title-vcenter"
+            dialogClassName='modal-80w modal-80h'
             centered
         >
             <Modal.Header closeButton>
@@ -26,6 +33,26 @@ export function CheckoutModal({ show, onHide }: CheckoutModalProps) {
                 <p>
                     Do you want to proceed checkout?
                 </p>
+                <div className='d-flex'>
+                    <div>
+                        {
+                            cartItems.map(item => (
+                                <CartItem {...item} />
+                            ))
+                        }
+                    </div>
+                    <div>
+                        Hello
+                    </div>
+                </div>
+                <div className='fw-bold d-flex justify-content-end'>
+                    <u>
+                        Total {formatCurrency(cartItems.reduce((total, cartItem) => {
+                            const item = storeItems.find(i => i.id === cartItem.id)
+                            return total + (item?.price || 0) * cartItem.quantity
+                        }, 0))}
+                    </u>
+                </div>
             </Modal.Body>
             <Modal.Footer>
                 <Button onClick={onHide}>Yes</Button>
