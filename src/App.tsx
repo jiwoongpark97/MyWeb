ffd4ae1e7b7e80572bf44439bfbc6ae02f7eef49
JP <jiwoongpark97@gmail.com>
2024-05-19 03:20:31 -0700

    Change to kotlin image for testing

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
           </Routes>
         </Container> */}
       <div className='vh-100 vw-100'>
-        <ImageSmudge imageUrl1={'/imgs/storeItems/javascript.png'} imageUrl2={'/imgs/storeItems/typescript.png'} />
+        <ImageSmudge imageUrl1={'/imgs/storeItems/javascript.png'} imageUrl2={'/imgs/storeItems/kotlin.png'} />
       </div>
       {/* </div> */}
     </ShoppingCartProvider>
