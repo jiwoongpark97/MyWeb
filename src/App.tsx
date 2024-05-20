b2ef1e8a946179b7f11887ff987ff6a7603727c8
JP <jiwoongpark97@gmail.com>
2024-05-19 23:32:06 -0700

    main screen css fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
-import { Home } from './pages/Home'
 import { About } from './pages/About'
 import { Store } from './pages/Store'
 import { Navbar } from './components/Navbar'
@@ -18,17 +17,16 @@ function App() {
 
   return (
     <ShoppingCartProvider>
-      <div className='vh-100 vw-100 d-flex justify-content-center align-items-center flex-column' style={{ width: '100%', height: '100%' }}>
+      <div className='vh-100 vw-100 d-flex justify-content-center align-items-center flex-column' style={{ boxSizing: 'border-box', margin: 0, padding: 0, overflow: 'hidden' }}>
         <Navbar />
-        {/* <Container fluid className='p-5'>
+        <Container fluid style={{ margin: 0, padding: 0 }}>
           <Routes>
-            <Route path='/' element={<Home />} />
+            <Route path='/' element={<ImageSmudge imageUrls={images} resetThreshold={0.8} />} />
             <Route path='/store' element={<Store />} />
             <Route path='/about' element={<About />} />
           </Routes>
-        </Container> */}
-
-        <ImageSmudge imageUrls={images} resetThreshold={0.6} />
+        </Container>
+        {/* <ImageSmudge imageUrls={images} resetThreshold={0.6} /> */}
 
       </div>
     </ShoppingCartProvider>
